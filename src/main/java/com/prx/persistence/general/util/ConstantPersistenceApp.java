f237fd42bc29ba484d24290982164806539dd660
Luis Mata <69470188+lanmata@users.noreply.github.com>
2024-11-19 18:34:34 -0500

    PRX-253 Upgrade findByAlias method in UserRepository to Include Role Information
    3. Reorder the modifiers to comply with the Java Language Specification

--- src/main/java/com/prx/persistence/general/util/ConstantPersistenceApp.java
+++ src/main/java/com/prx/persistence/general/util/ConstantPersistenceApp.java
@@ -2,26 +2,26 @@ package com.prx.persistence.general.util;
 
 public final class ConstantPersistenceApp {
 
-    public final static String PG_UUID_FUNCTION = "general.uuid_generate_v4()";
+    public static final String PG_UUID_FUNCTION = "general.uuid_generate_v4()";
 
-    public final static String SCHEMA_NAME = "general";
+    public static final String SCHEMA_NAME = "general";
 
-    public final static String ADDRESS_TABLE_NAME = "address";
-    public final static String CONTACT_TABLE_NAME = "contact";
-    public final static String CONTACT_TYPE_TABLE_NAME = "contact_type";
-    public final static String FEATURE_TABLE_NAME = "feature";
-    public final static String IDENTIFICATION_DOCUMENT_TABLE_NAME = "identification_document";
-    public final static String PERSON_TABLE_NAME = "person";
-    public final static String ROLE_TABLE_NAME = "role";
-    public final static String ROLE_FEATURE_TABLE_NAME = "role_feature";
-    public final static String SERVICE_TABLE_NAME ="service";
-    public final static String USER_TABLE_NAME = "user";
-    public final static String USER_ROLE_TABLE_NAME ="user_role";
+    public static final String ADDRESS_TABLE_NAME = "address";
+    public static final String CONTACT_TABLE_NAME = "contact";
+    public static final String CONTACT_TYPE_TABLE_NAME = "contact_type";
+    public static final String FEATURE_TABLE_NAME = "feature";
+    public static final String IDENTIFICATION_DOCUMENT_TABLE_NAME = "identification_document";
+    public static final String PERSON_TABLE_NAME = "person";
+    public static final String ROLE_TABLE_NAME = "role";
+    public static final String ROLE_FEATURE_TABLE_NAME = "role_feature";
+    public static final String SERVICE_TABLE_NAME ="service";
+    public static final String USER_TABLE_NAME = "user";
+    public static final String USER_ROLE_TABLE_NAME ="user_role";
 
-    public final static String ID_CN = "id";
-    public final static String NAME_CN = "name";
-    public final static String ACTIVE_CN = "active";
-    public final static String DESCRIPTION_CN = "description";
+    public static final String ID_CN = "id";
+    public static final String NAME_CN = "name";
+    public static final String ACTIVE_CN = "active";
+    public static final String DESCRIPTION_CN = "description";
 
     private ConstantPersistenceApp() {
         //Default constructor
